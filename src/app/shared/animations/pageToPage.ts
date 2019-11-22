49dc40b642d74eaeaeed2e863e98bda6ae585b1b
Felix Hofmann <felix.hofmann@rustyy.de>
2019-11-22 21:34:43 +0100

    fix: error pageToPage-transition

--- src/app/shared/animations/pageToPage.ts
+++ src/app/shared/animations/pageToPage.ts
@@ -3,20 +3,24 @@ import { BEZIER } from './bezier';
 
 export const pageToPage = animation([
   style({ position: 'relative' }),
-  query(':enter, :leave', [
-    style({
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100vw',
-      height: '100vh'
-    })
-  ]),
+  query(
+    ':enter, :leave',
+    [
+      style({
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh'
+      })
+    ],
+    { optional: true }
+  ),
   query(':enter', [style({ transform: 'translateX(100vw)', opacity: 0 })]),
-  query(':leave', [style({ transform: 'translateX(0)', opacity: 1 })]),
+  query(':leave', [style({ transform: 'translateX(0)', opacity: 1 })], { optional: true }),
 
   group([
     query(':enter', [animate(BEZIER, style({ transform: 'translateX(0)', opacity: 1 }))]),
-    query(':leave', [animate(BEZIER, style({ transform: 'translateX(-100vw)', opacity: 0 }))])
+    query(':leave', [animate(BEZIER, style({ transform: 'translateX(-100vw)', opacity: 0 }))], { optional: true })
   ])
 ]);
