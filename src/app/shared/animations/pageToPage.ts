34d1ebff41a747aa3d5c1d81cfa84ade88d9e106
Felix Hofmann <felix.hofmann@rustyy.de>
2019-12-10 21:09:49 +0100

    fix: page to page transition on larger screens

--- src/app/shared/animations/pageToPage.ts
+++ src/app/shared/animations/pageToPage.ts
@@ -10,7 +10,7 @@ export const pageToPage = animation([
         position: 'absolute',
         top: 0,
         left: 0,
-        width: '100vw',
+        width: '100%',
         height: '100vh'
       })
     ],
