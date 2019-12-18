72159800e28a1d9709c08a5854b607494c5a22cc
Felix Hofmann <felix.hofmann@rustyy.de>
2019-12-18 18:10:36 +0100

    refactor: reduce duplicates in minutes-hours pipe

--- src/app/shared/pipes/minutes-to-hours.pipe.ts
+++ src/app/shared/pipes/minutes-to-hours.pipe.ts
@@ -1,15 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { HelperService } from '../helper/helper.service';
 
 @Pipe({
   name: 'minutesToHours'
 })
 export class MinutesToHoursPipe implements PipeTransform {
-  transform(value: number, args?: any): any {
-    const hours = Math.floor(value / 60);
-    const hoursString = hours === 0 ? '00' : hours < 10 ? `0${hours}` : hours;
-    const minutes = value % 60;
-    const minutesString = minutes === 0 ? '00' : minutes < 10 ? `0${minutes}` : minutes;
+  constructor(private helperService: HelperService) {}
 
-    return `${hoursString}:${minutesString}`;
+  transform(value: number, args?: any): any {
+    return this.helperService.minutesToHhMm(value);
   }
 }
