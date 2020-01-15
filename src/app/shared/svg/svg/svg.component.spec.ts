945df5a81fc291b62f53b253f6b27d55e3c65baa
Felix Hofmann <felix.hofmann@rustyy.de>
2020-01-15 22:23:02 +0100

    test: svg component

--- src/app/shared/svg/svg/svg.component.spec.ts
+++ src/app/shared/svg/svg/svg.component.spec.ts
@@ -10,15 +10,26 @@ describe('SvgComponent', () => {
     TestBed.configureTestingModule({
       declarations: [SvgComponent]
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(SvgComponent);
     component = fixture.componentInstance;
+
+    component.name = 'foo';
+    component.size = 'small';
+
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the correct size-modifier', () => {
+    expect(fixture.nativeElement.classList.contains('app-svg--small')).toBeTrue();
+  });
+
+  it('should refer to the correct symbol in xlink:href', () => {
+    const useEl = fixture.nativeElement.querySelector('use');
+    expect(useEl.getAttribute('xlink:href')).toBe('#symbol-icon-foo');
   });
 });
