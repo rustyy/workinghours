9ba41d0ca3a035a9697400a465570c8006b3b85c
Felix Hofmann <felix.hofmann@rustyy.de>
2020-01-15 21:57:37 +0100

    test: week-indicator component

--- src/app/record-list/week-indicator/week-indicator.component.spec.ts
+++ src/app/record-list/week-indicator/week-indicator.component.spec.ts
@@ -1,24 +1,44 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Pipe, PipeTransform } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { WeekIndicatorComponent } from './week-indicator.component';
 
 describe('WeekIndicatorComponent', () => {
-  let component: WeekIndicatorComponent;
-  let fixture: ComponentFixture<WeekIndicatorComponent>;
+  @Pipe({ name: 'translate' })
+  class MockedTranslatePipe implements PipeTransform {
+    transform(value: any, ...args): any {
+      return value;
+    }
+  }
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [WeekIndicatorComponent]
-    }).compileComponents();
-  }));
+  let fixture: ComponentFixture<WeekIndicatorComponent>;
+  let component: WeekIndicatorComponent;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [WeekIndicatorComponent, MockedTranslatePipe]
+    });
+
     fixture = TestBed.createComponent(WeekIndicatorComponent);
     component = fixture.componentInstance;
+
+    component.year = 2020;
+    component.week = 50;
+    component.from = '1577833200000'; // 1.1.2020
+    component.to = '1578178799000'; // 4.1.2020
+
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('should have correct textContent', () => {
+    expect(fixture.nativeElement.textContent).toBe('W 50\n01.01. - 04.01. 2020');
+  });
+
+  it('should have one ".text-settled" with correct textContent', () => {
+    expect(fixture.nativeElement.children.length).toEqual(1);
+    expect(fixture.nativeElement.querySelector('.text--settled').textContent).toBe('01.01. - 04.01. 2020');
   });
 });
