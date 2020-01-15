1f355d7a9987a346645cc0290e9487e9d581a2d4
Felix Hofmann <felix.hofmann@rustyy.de>
2020-01-15 20:28:45 +0100

    test: minutes-to-hours pipe

--- src/app/shared/pipes/minutes-to-hours.pipe.spec.ts
+++ src/app/shared/pipes/minutes-to-hours.pipe.spec.ts
@@ -1,8 +1,27 @@
+import { TestBed } from '@angular/core/testing';
 import { MinutesToHoursPipe } from './minutes-to-hours.pipe';
+import { HelperService } from '../helper/helper.service';
 
-describe('MinutesToHoursPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MinutesToHoursPipe();
-    expect(pipe).toBeTruthy();
+describe('minutes-to-hours.pipe', () => {
+  let pipe: MinutesToHoursPipe;
+  let helperServiceSpy: jasmine.SpyObj<HelperService>;
+
+  beforeEach(() => {
+    const spy = jasmine.createSpyObj('HelperService', ['minutesToHhMm']);
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: HelperService, useValue: spy }]
+    });
+
+    helperServiceSpy = TestBed.get(HelperService);
+    pipe = new MinutesToHoursPipe(helperServiceSpy);
+  });
+
+  it('calls HelperService.minutesToHhMm to transform number to time-string', () => {
+    const stubValue = '01:30';
+    helperServiceSpy.minutesToHhMm.and.returnValue(stubValue);
+
+    expect(pipe.transform(90)).toBe(stubValue);
+    expect(helperServiceSpy.minutesToHhMm.calls.count()).toBe(1);
   });
 });
