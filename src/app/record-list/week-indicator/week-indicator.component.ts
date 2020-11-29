0a642d07835f498984bbe86d2385430cc31bb3a4
Felix Hofmann <felix.hofmann@rustyy.de>
2020-11-29 20:17:33 +0100

    refactor: code style

--- src/app/record-list/week-indicator/week-indicator.component.ts
+++ src/app/record-list/week-indicator/week-indicator.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-week-indicator',
   templateUrl: './week-indicator.component.html',
-  styleUrls: ['./week-indicator.component.scss']
+  styleUrls: ['./week-indicator.component.scss'],
 })
 export class WeekIndicatorComponent {
   @Input() year: number;
