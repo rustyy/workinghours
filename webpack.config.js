9369fdf612e8d6fc2324d61de2a0d7bc8dc2bd14
Felix Hofmann <felix.hofmann@rustyy.de>
2021-03-04 00:55:21 +0100

    perf: reduce moment locales

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+const MomentLocalePlugin = require('moment-locales-webpack-plugin');
+
 module.exports = (config, options, targetOptions) => {
+  config.plugins.push(
+    new MomentLocalePlugin({
+      localesToKeep: ['de'],
+    })
+  );
+
   return config;
 };
