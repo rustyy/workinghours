44e6a7147ceeae7cd1e84255ab81afcc2e4cc493
Felix Hofmann <felix.hofmann@rustyy.de>
2021-03-19 22:29:59 +0100

    refactor: remove empty week-indicator.component.scss

--- src/app/record-list/week-indicator/week-indicator.component.ts
+++ src/app/record-list/week-indicator/week-indicator.component.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-week-indicator',
   templateUrl: './week-indicator.component.html',
-  styleUrls: ['./week-indicator.component.scss'],
 })
 export class WeekIndicatorComponent {
   // W52 2020 - 12/21 - 12/27
